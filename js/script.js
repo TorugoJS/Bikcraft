031e11db6f58c6e53ad468c3b510a2ed44686edc
Victor <victor.devfrontend@gmail.com>
2023-03-21 10:46:11 -0300

    bikcraft

--- js/script.js
+++ js/script.js
@@ -18,10 +18,27 @@ function ativarProduto(parametro) {
   if (elemento) {
     elemento.checked = true;
   }
-
-  console.log(elemento);
 }
-
 parametros.forEach(ativarProduto);
 
-console.log(parametros);
+//perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+console.log(perguntas);
