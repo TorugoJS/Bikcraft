90f7d99564d4800d87bb69c1152c8b0db626fb85
Victor <victor.devfrontend@gmail.com>
2023-03-21 11:10:04 -0300

    bikcraft

--- js/script.js
+++ js/script.js
@@ -42,3 +42,23 @@ function eventosPerguntas(pergunta) {
 perguntas.forEach(eventosPerguntas);
 
 console.log(perguntas);
+
+// galeria de bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeiraContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width:1000px)").matches;
+  console.log(media);
+
+  if (media) {
+    galeiraContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
